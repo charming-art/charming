4a583b4bde445e98e58c4ba3cb54f9328f651fc0
Bairui Su <pearminipro@gmail.com>
2024-01-20 13:30:22 +0800

    Fix cm.randomInt (#221)

--- src/math/random.js
+++ src/math/random.js
@@ -1,4 +1,4 @@
-export function random(a, b) {
-  const [min = 0, max = 1] = arguments.length === 1 ? [0, a] : [a, b];
+export function random(min, max) {
+  [min = 0, max = 1] = arguments.length === 1 ? [0, min] : [min, max];
   return min + (max - min) * Math.random();
 }
--- src/math/randomInt.js
+++ src/math/randomInt.js
@@ -1,5 +1,6 @@
-import { random } from "./random.js";
-
 export function randomInt(min, max) {
-  return Math.floor(random(min, max));
+  [min = 0, max = 1] = arguments.length === 1 ? [0, min] : [min, max];
+  min = Math.floor(min);
+  max = Math.floor(max) - min;
+  return Math.floor(Math.random() * max + min);
 }
